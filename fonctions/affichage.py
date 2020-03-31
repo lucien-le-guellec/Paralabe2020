05575f6e90ac6a8d4f3a5959cbc0d9a4d7491628
LucienLeGuellec <lucien.le-guellec@orange.fr>
2020-03-31 13:28:45 +0200

    évolution clustering + affichage

--- fonctions/affichage.py
+++ fonctions/affichage.py
@@ -1,14 +1,38 @@
 import matplotlib.pyplot as plt
-import json
+import colorsys
 
-from extraction.extracteur_pixels import charger_data
+from extraction.extracteur_pixels import charger_data as cd1
+from extraction.ouvrir_json import charger_data
 from fonctions.hdbscan_clustering import clusteriser
 from fonctions.umap_reduction import reduire_dimensions
 
 
-def afficher(chemin, clustering):
+def afficher(chemin, nom, clustering):
     data = charger_data([chemin+clustering["fichier"]])
+    n_clusters = len(clustering["clusters"])-1
+    if clustering["dimensions"]>2:
+        return "Affichage impossible avec plus de deux dimensions"
+    else:
+        x = []
+        y = []
+        couleurs = []
+        for i in data:
+            if i["nom"] in clustering["clusters"]["-1"]:
+                x.append(i["descripteurs"][clustering["descripteurs"]][0])
+                y.append(i["descripteurs"][clustering["descripteurs"]][1])
+                couleurs.append((0.8, 0.8, 0.8))
+            else:
+                for j in range(n_clusters):
+                    if i["nom"] in clustering["clusters"][str(j)]:
+                        x.append(i["descripteurs"][clustering["descripteurs"]][0])
+                        y.append(i["descripteurs"][clustering["descripteurs"]][1])
+                        couleurs.append(colorsys.hsv_to_rgb(1/(n_clusters)*j,1,1))
+        plt.scatter(x, y, c=couleurs, s=0.1)
+        plt.rcParams["axes.titlesize"] = 8
+        plt.title(clustering["description"])
+        plt.savefig(chemin+nom+".png")
+        plt.show()
 
 if __name__ == '__main__':
-    data = reduire_dimensions(charger_data(["../data/imagettes_paradiit/", 20, "data.json"]), "pixels", "2d", 100, 0.1, 2, "../data/imagettes_paradiit/data.json")
-    afficher(clusteriser(data, "2d", "clusters1", 10, "../data/imagettes_paradiit/", "data.json"))
+    data = reduire_dimensions(cd1(["../data/imagettes_paradiit/", 20, "data.json"]), "pixels", "2d", 100, 0.1, 2, "../data/imagettes_paradiit/data.json")
+    afficher("../data/imagettes_paradiit/", "clusters1", clusteriser(data, "2d", "clusters1", 10, "../data/imagettes_paradiit/", "data.json"))
--- fonctions/hdbscan_clustering.py
+++ fonctions/hdbscan_clustering.py
@@ -10,16 +10,19 @@ def clusteriser(data, nom_entree, nom_sortie, min_samples, path, nom_fichier):
     date = datetime.now()
     nombre_dimensions = len(data[0]["descripteurs"][nom_entree])
     vecteurs = []
-    clustering = {"description": "Clustering fait le "+date.strftime("%d/%m/%Y, %H:%M:%S")
-                                + ", depuis les descripteurs "+nom_entree+" ("+str(nombre_dimensions)
-                                + " dimensions) du fichier "+nom_fichier,
-                  "fichier": nom_fichier,
-                  "clusters": {},
-                  "tags": {}}
     for i in data:
         vecteurs.append(i["descripteurs"][nom_entree])
     clusterer = hdbscan.HDBSCAN(min_samples=min_samples)
     clusterer.fit(vecteurs)
+    clustering = {"description": "Clustering fait le "+date.strftime("%d/%m/%Y, %H:%M:%S")
+                                + ", depuis les descripteurs \""+nom_entree+"\" ("+str(nombre_dimensions)
+                                + " dimensions) \n du fichier "+nom_fichier+", avec min_samples = "+str(min_samples),
+                  "fichier": nom_fichier,
+                  "descripteurs": nom_entree,
+                  "dimensions": nombre_dimensions,
+                  "min_samples": min_samples,
+                  "clusters": {},
+                  "tags": {}}
     for i in range(len(data)):
         cluster = clusterer.labels_[i]
         if str(cluster) not in clustering["clusters"]:
